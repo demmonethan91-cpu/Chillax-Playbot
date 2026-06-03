472c1652297091629808be78a38811ebb2675ec6
demmonethan91-cpu <demmonethan91@gmail.com>
2026-06-02 17:28:13 -0700

    Update bot activity message and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "use /help for list of commands",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 1, 
       },
     ],
   },
